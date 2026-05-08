2fe9c32f39eeb867a45a5f4ce6d6304d6dac895a
itohansan <itohanidubor@example.com>
2026-05-08 17:39:49 -0400

    car models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,15 +13,16 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
-
-
+from .models import CarMake, CarModel
+from .populate import initiate
+ 
+ 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
-
+ 
+ 
 # Create your views here.
-
+ 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
@@ -37,29 +38,43 @@ def login_user(request):
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
-
+ 
+ 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
-
+ 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
 # def registration(request):
 # ...
-
-# # Update the `get_dealerships` view to render the index page with
+ 
+# Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 # def get_dealerships(request):
 # ...
-
+ 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
 # ...
-
+ 
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_details(request, dealer_id):
 # ...
-
+ 
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+ 
+ 
+# Get cars method
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels": cars})
